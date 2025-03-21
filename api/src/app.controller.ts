18b480efc9d50527550420a9eb5665283439ca15
Richard Nguyen <richard@richardhnguyen.com>
2025-03-21 12:38:19 -0700

    chore: update linting and formatting options

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
+  // eslint-disable-next-line prettier/prettier
   constructor(private readonly appService: AppService) {}
 
   @Get()
