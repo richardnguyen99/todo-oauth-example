4bf10aa4a6e637492d787e1d08e7401ca495480a
Richard Nguyen <richard@richardhnguyen.com>
2025-05-12 08:49:07 -0700

    feat(web): add workspace item

---
+++ web/src/app/dashboard/workspace/_components/workspace-sidebar/workspace-item.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React, { type JSX } from "react";
+import Link from "next/link";
+import * as LucideIcons from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Color } from "@/_types/color";
+import { cn } from "@/lib/utils";
+import { colorMap } from "../../_constants/colors";
+import { Workspace } from "@/_types/workspace";
+
+type Props = Readonly<
+  {
+    workspace: Workspace;
+    isActive: boolean;
+  } & React.ComponentProps<typeof SidebarMenuItem>
+>;
+
+export default function SidebarWorkspaceItem({
+  workspace,
+  isActive,
+  ...props
+}: Props): JSX.Element {
+  const { isMobile, open } = useSidebar();
+
+  const renderIcon = (icon: string, color: Color) => {
+    const Icon = LucideIcons[
+      icon as keyof typeof LucideIcons
+    ] as React.ComponentType<LucideIcons.LucideProps>;
+
+    return (
+      <div
+        className={cn(
+          "flex aspect-square size-7 items-center justify-center rounded-md",
+          colorMap[color],
+        )}
+      >
+        <Icon className="size-5" />
+      </div>
+    );
+  };
+
+  return (
+    <SidebarMenuItem {...props}>
+      <SidebarMenuButton
+        asChild
+        className={cn(
+          "group-hover/menu-item:!bg-sidebar-accent group-data-[collapsible=icon]:!p-0",
+          {
+            "bg-sidebar-accent/40": isActive,
+          },
+        )}
+        tooltip={!open ? workspace.title : undefined}
+      >
+        <Link href={`/dashboard/workspace/${workspace._id}`}>
+          {renderIcon(workspace.icon, workspace.color)}
+          <span>{workspace.title}</span>
+        </Link>
+      </SidebarMenuButton>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <SidebarMenuAction className="opacity-0 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100">
+            <LucideIcons.MoreHorizontal />
+            <span className="sr-only">More</span>
+          </SidebarMenuAction>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="w-48 rounded-lg"
+          side={isMobile ? "bottom" : "right"}
+          align={isMobile ? "end" : "start"}
+        >
+          <DropdownMenuItem>
+            <LucideIcons.Folder className="text-muted-foreground" />
+            <span>View Project</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LucideIcons.Forward className="text-muted-foreground" />
+            <span>Share Project</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LucideIcons.Trash2 className="text-muted-foreground" />
+            <span>Delete Project</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </SidebarMenuItem>
+  );
+}
--- web/src/app/dashboard/workspace/_components/workspace-sidebar/workspace-list.tsx
+++ web/src/app/dashboard/workspace/_components/workspace-sidebar/workspace-list.tsx
@@ -5,104 +5,62 @@ import Link from "next/link";
 import * as LucideIcons from "lucide-react";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   SidebarGroup,
+  SidebarGroupAction,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
 import { useWorkspaceStore } from "@/app/dashboard/_providers/workspace";
-import { Color } from "@/_types/color";
-import { cn } from "@/lib/utils";
-import { colorMap } from "../../_constants/colors";
+import SidebarWorkspaceItem from "./workspace-item";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function SidebarWorkspaceList(): JSX.Element {
   const { workspaces, activeWorkspace } = useWorkspaceStore((s) => s);
-  const { isMobile, open } = useSidebar();
-
-  const renderIcon = (icon: string, color: Color) => {
-    const Icon = LucideIcons[
-      icon as keyof typeof LucideIcons
-    ] as React.ComponentType<LucideIcons.LucideProps>;
-
-    return (
-      <div
-        className={cn(
-          "flex aspect-square size-7 items-center justify-center rounded-md",
-          colorMap[color],
-        )}
-      >
-        <Icon className="size-5" />
-      </div>
-    );
-  };
+  const { isMobile } = useSidebar();
 
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Projects</SidebarGroupLabel>
+      <SidebarGroupLabel className="peer/group-label">
+        Your workspaces
+      </SidebarGroupLabel>
+      <Tooltip disableHoverableContent>
+        <TooltipTrigger asChild>
+          <SidebarGroupAction className="opacity-0 transition-opacity duration-100 peer-hover/group-label:opacity-100 hover:opacity-100">
+            <span className="sr-only">Your workspaces</span>
+            <LucideIcons.Plus />
+          </SidebarGroupAction>
+        </TooltipTrigger>
+        <TooltipContent
+          side={isMobile ? "bottom" : "right"}
+          align={isMobile ? "end" : "start"}
+        >
+          Add workspaces
+        </TooltipContent>
+      </Tooltip>
+
       <SidebarMenu>
         {workspaces.slice(0, 5).map((ws) => (
-          <SidebarMenuItem key={ws._id} className="relative z-10">
-            <SidebarMenuButton
-              asChild
-              className={cn(
-                "group-hover/menu-item:!bg-sidebar-accent group-data-[collapsible=icon]:!p-0",
-                {
-                  "bg-sidebar-accent/40": activeWorkspace?._id === ws._id,
-                },
-              )}
-              tooltip={!open ? ws.title : undefined}
-            >
-              <Link href={`/dashboard/workspace/${ws._id}`}>
-                {renderIcon(ws.icon, ws.color)}
-                <span>{ws.title}</span>
-              </Link>
-            </SidebarMenuButton>
-
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuAction className="opacity-0 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100">
-                  <LucideIcons.MoreHorizontal />
-                  <span className="sr-only">More</span>
-                </SidebarMenuAction>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="w-48 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align={isMobile ? "end" : "start"}
-              >
-                <DropdownMenuItem>
-                  <LucideIcons.Folder className="text-muted-foreground" />
-                  <span>View Project</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <LucideIcons.Forward className="text-muted-foreground" />
-                  <span>Share Project</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <LucideIcons.Trash2 className="text-muted-foreground" />
-                  <span>Delete Project</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
+          <SidebarWorkspaceItem
+            key={ws._id}
+            workspace={ws}
+            isActive={ws._id === activeWorkspace?._id}
+          />
         ))}
 
         {workspaces.length > 5 && (
           <SidebarMenuItem>
-            <SidebarMenuButton className="text-sidebar-foreground/70">
-              <LucideIcons.MoreHorizontal className="text-sidebar-foreground/70" />
-              <span>{workspaces.length - 5} more</span>
+            <SidebarMenuButton className="text-sidebar-foreground/70" asChild>
+              <Link href={`/dashboard/workspaces`}>
+                <LucideIcons.MoreHorizontal className="text-sidebar-foreground/70" />
+                <span>{workspaces.length - 5} more</span>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         )}
