03993ee900b3f72c97b202aee0f0b545741b9876
Richard Nguyen <richard@richardhnguyen.com>
2025-06-16 08:50:19 -0700

    feat(web): implement adding new members form

--- web/src/app/dashboard/workspace/[workspace]/members/_components/member-add-dialog.tsx
+++ web/src/app/dashboard/workspace/[workspace]/members/_components/member-add-dialog.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import React, { type JSX } from "react";
-import { UserPlus } from "lucide-react";
+import { Check, ChevronsUpDown, UserPlus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -12,40 +15,162 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+
+const FormSchema = z.object({
+  language: z.string({
+    required_error: "Please select a language.",
+  }),
+});
+
+type FormData = z.infer<typeof FormSchema>;
+
+const languages = [
+  { label: "English", value: "en" },
+  { label: "French", value: "fr" },
+  { label: "German", value: "de" },
+  { label: "Spanish", value: "es" },
+  { label: "Portuguese", value: "pt" },
+  { label: "Russian", value: "ru" },
+  { label: "Japanese", value: "ja" },
+  { label: "Korean", value: "ko" },
+  { label: "Chinese", value: "zh" },
+] as const;
 
 export default function MemberAddDialog(): JSX.Element {
+  const [loading] = React.useState(false);
   const [open, setOpen] = React.useState(false);
+  const form = useForm<FormData>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = React.useCallback((data: FormData) => {
+    console.log(data);
+  }, []);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="secondary" className="p-2">
-          <UserPlus className="h-4 w-4 sm:mr-1" />
-          <span className="hidden sm:inline">Invite Member</span>
-        </Button>
-      </DialogTrigger>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <DialogTrigger asChild>
+            <Button variant="secondary" className="p-2">
+              <UserPlus className="h-4 w-4 sm:mr-1" />
+              <span className="hidden sm:inline">Invite Member</span>
+            </Button>
+          </DialogTrigger>
 
-      <DialogContent>
-        <DialogTitle>Invite Member</DialogTitle>
-        <DialogDescription>
-          Invite new members to your workspace by entering their email
-          addresses. They will receive an invitation to join and collaborate on
-          projects.
-        </DialogDescription>
+          <DialogContent>
+            <DialogTitle>Invite Member</DialogTitle>
+            <DialogDescription>
+              Invite new members to your workspace to work with you.
+            </DialogDescription>
 
-        {/* Form fields for inviting members would go here */}
-        <form>
-          <Input placeholder="Email address" />
+            <FormField
+              control={form.control}
+              name="language"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Language</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          role="combobox"
+                          className={cn(
+                            "w-full justify-between",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value
+                            ? languages.find(
+                                (language) => language.value === field.value,
+                              )?.label
+                            : "Select language"}
+                          <ChevronsUpDown className="opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+
+                    <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0">
+                      <Command>
+                        <CommandInput
+                          placeholder="Search framework..."
+                          className="h-9"
+                        />
+                        <CommandList>
+                          <CommandEmpty>No framework found.</CommandEmpty>
+                          <CommandGroup>
+                            {languages.map((language) => (
+                              <CommandItem
+                                value={language.label}
+                                key={language.value}
+                                onSelect={() => {
+                                  form.setValue("language", language.value);
+                                }}
+                              >
+                                {language.label}
+                                <Check
+                                  className={cn(
+                                    "ml-auto",
+                                    language.value === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0",
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormDescription>
+                    This is the language that will be used in the dashboard.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button
+                onClick={() => {
+                  setOpen(false);
+                  form.reset();
+                }}
+              >
+                Cancel
+              </Button>
+              <Button type="submit" disabled={loading}>
+                Submit
+              </Button>
+            </DialogFooter>
+          </DialogContent>
         </form>
-
-        <DialogFooter>
-          <Button variant="secondary" onClick={() => setOpen(false)}>
-            Cancel
-          </Button>
-          <Button type="submit">Send Invite</Button>
-        </DialogFooter>
-      </DialogContent>
+      </Form>
     </Dialog>
   );
 }
